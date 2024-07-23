2b711bc36bdd16cb3b73d363af6cefea1f17f4af
Alexander Mulyar <mulyar.alexander0503@yandex.ru>
2024-07-23 16:27:22 +0500

    First commit

---
+++ conf.php
@@ -0,0 +1,12 @@
+<?php
+    $CONF["company"] = "AMServices";
+    $CONF["copyright"] = "© 2024 Alexander Mulyar";
+
+    $CONF["db_host"] = "127.0.0.1";
+    $CONF["db_user"] = "amservicesdb";
+    $CONF["db_passwd"] = "amservicesdb";
+    $CONF["db_database"] = "amservicesdb";
+
+    $CONF["enable_amsfiles"] = true;
+    $CONF["enable_amsnotes"] = true;
+?>
---
+++ index.php
@@ -0,0 +1,85 @@
+<?php
+    include "conf.php";
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> <?php print($CONF["company"]); ?> </title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="img/favicon.png">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/index.css">
+    </head>
+
+    <body>
+        <div class="container">
+            <header>
+                <div class="header_logo">
+                    <a href="./"> <img src="img/favicon.png" width="75px" height="75px"> </a>
+                </div>
+                <div class="header_link">
+                    <a href="./"> <b> <i><?php print($CONF["company"]); ?></i> </b> </a>
+                </div>
+            </header>
+
+            <div class="siteMap">
+                <i> <a href="./">Главная</a> </i>
+            </div>
+
+            <div class="link_all">
+                <div class="link_services">
+                    <i> <a href="#">PasswordGenerator</a> </i>
+                    <p>Простой генератор паролей</p>
+                    <hr>
+                    <i> <a href="#">Factor</a> </i>
+                    <p>Разложение числа на множители</p>
+                    <hr>
+                    <i> <a href="#">Random</a> </i>
+                    <p>Генератор чисел и имен</p>
+                    <hr>
+                    <i> <a href="#">Color</a> </i>
+                    <p>RGB-палитра</p>
+                    <hr>
+                    <i> <a href="#">Anecdotes</a></i>
+                    <p>Сборник анекдотов</p>
+                </div>
+
+                <div class="link_games">
+                    <i> <a href="#">TicTacToe</a></i>
+                    <p>Игра "Крестики нолики"</p>
+                    <hr>
+                    <i> <a href="#">Tag</a> </i>
+                    <p>Игра "Пятнашки"</p>
+                    <hr>
+                    <i> <a href="#">Schulte</a> </i>
+                    <p>Таблицы Шульте</p>
+                    <hr>
+                </div>
+            </div>
+
+            <?php
+                if ($CONF["enable_amsfiles"] == true)
+                {
+                    print("<div class='amsfiles_link'>");
+                    print("<img src='img/AMSFiles.png'  width='50px' height='50px'>");
+                    print("<a href='#'>AMSFiles</a>");
+                    print("</div>");
+                }
+
+                if ($CONF["enable_amsnotes"] == true)
+                {
+                    print("<div class='amsnotes_link'>");
+                    print("<img src='img/AMSNotes.png' width='50px' height='50px'>");
+                    print("<a href='#'>AMSNotes</a>");
+                    print("</div>");
+                }
+            ?>
+
+            <footer>
+                <p> <i> <?php print($CONF["copyright"]); ?> </i> </p>
+            </footer>
+        </div>
+    </body>
+</html>
